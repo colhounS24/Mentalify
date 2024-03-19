c62e6ed7fc570141130bb2bd6f6f965ff1f1c0ed
Sean Colhoun <sean.colhoun.2020@mumail.ie>
2024-03-19 19:56:52 +0000

    Trying to fix Links

--- mentalify/src/App.tsx
+++ mentalify/src/App.tsx
@@ -17,17 +17,17 @@ function App() {
   return (
     <BrowserRouter basename={baseurl}>
       <Routes>
-        <Route path="/" element={<Splash></Splash>}></Route>
-        <Route path="login" element={<LoginBoxForm></LoginBoxForm>}></Route>
-        <Route path="signup" element={<SignUp></SignUp>}></Route>
-        <Route path="homepage" element={<Homepage></Homepage>}></Route>
-        <Route path="education" element={<Education></Education>}></Route>
-        <Route path="training" element={<MemoryGame1></MemoryGame1>}></Route>
+        <Route path="" element={<Splash></Splash>}></Route>
+        <Route path="/login" element={<LoginBoxForm></LoginBoxForm>}></Route>
+        <Route path="/signup" element={<SignUp></SignUp>}></Route>
+        <Route path="/homepage" element={<Homepage></Homepage>}></Route>
+        <Route path="/education" element={<Education></Education>}></Route>
+        <Route path="/training" element={<MemoryGame1></MemoryGame1>}></Route>
         <Route path="*" element={<NotFound></NotFound>}></Route>
         <Route path="/newsletter" element={<NewsLetter></NewsLetter>}></Route>
 
         <Route
-          path="forgot"
+          path="/forgot"
           element={<ForgotPassword></ForgotPassword>}
         ></Route>
       </Routes>
